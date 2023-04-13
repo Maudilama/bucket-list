99da2bcee36a57703d6cee27847fe9483ebb9630
Maud <marieaude.torchy2022@campus-eni.fr>
2023-04-13 18:49:24 +0200

    TP module 5

--- src/Controller/WishController.php
+++ src/Controller/WishController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\WishRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,20 +12,23 @@ class WishController extends AbstractController
     /**
      * @Route("/wish", name= "wish_list")
      */
-    public function list(): Response
+    public function list(WishRepository $wishRepository): Response
     {
+        $wishes = $wishRepository->findWishes();
         return $this->render('wish/list.html.twig', [
-
+            "wishes" => $wishes,
         ]);
     }
 
     /**
      * @Route("/wish/detail/{id}", name= "wish_details", requirements={"id"="\d+"})
      */
-    public function details(int $id): Response
+    public function details(int $id, WishRepository $wishRepository): Response
     {
-        return $this->render("wish/details.html.twig", [
+        $wish = $wishRepository->find($id);
 
+        return $this->render("wish/details.html.twig", [
+            "wish" => $wish,
         ]);
     }
 
---
+++ src/Repository/WishRepository.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Wish;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<Wish>
+ *
+ * @method Wish|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Wish|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Wish[]    findAll()
+ * @method Wish[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class WishRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Wish::class);
+    }
+
+    public function save(Wish $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->persist($entity);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+
+    public function remove(Wish $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->remove($entity);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+
+    public function findWishes(){
+        $queryBuilder = $this->createQueryBuilder('w');
+        $queryBuilder->addOrderBy('w.dateCreated', 'DESC');
+        $query = $queryBuilder->getQuery();
+        return $query->getResult();
+    }
+}
