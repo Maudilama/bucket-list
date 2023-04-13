20bb29d27f7c59c3b9600249707e611aaf708a0d
Maud <marieaude.torchy2022@campus-eni.fr>
2023-04-13 10:39:37 +0200

    TP module 4

---
+++ src/Controller/WishController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class WishController extends AbstractController
+{
+    /**
+     * @Route("/wish", name= "wish_list")
+     */
+    public function list(): Response
+    {
+        return $this->render('wish/list.html.twig', [
+
+        ]);
+    }
+
+    /**
+     * @Route("/wish/detail/{id}", name= "wish_details", requirements={"id"="\d+"})
+     */
+    public function details(int $id): Response
+    {
+        return $this->render("wish/details.html.twig", [
+
+        ]);
+    }
+
+
+}
--- src/Controller/mainController.php
+++ src/Controller/mainController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class mainController extends AbstractController
@@ -10,9 +11,23 @@ class mainController extends AbstractController
     /**
      * @Route("/", name="main_home")
      */
-    public function home()
+    public function home(): Response
     {
         return $this->render('main/home.html.twig');
+    }
+
+    /**
+     * @Route("/about", name="main_about")
+     */
+
+    public function about(): Response
+    {
+        $rawData = file_get_contents("../data/team.json");
+        $teamMembers = json_decode($rawData, true);
+
+        return $this->render('main/about.html.twig', [
+            'teamMembers' => $teamMembers
+        ]);
     }
 
 }
